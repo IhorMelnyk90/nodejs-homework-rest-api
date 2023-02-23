d3ac9f07535fb9ae19265176291e68373de87e5d
Igor Melnyk <igor.melnyk2011@gmail.com>
2023-02-23 22:39:12 +0200

    new fix by route addContact

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,7 +4,7 @@ const ctrl = require("../../controllers/contacts");
 
 const { ctrlWrapper } = require("../../helpers");
 
-const {validateBody} = require("../../middlewares");
+const validateBody = require("../../middlewares");
 
 const schemas = require("../../schemas/contacts");
 
@@ -15,6 +15,8 @@ const router = express.Router();
 router.get("/", ctrlWrapper(ctrl.getAll));
 
 // router.get("/:contactId", ctrlWrapper(ctrl.getById));
+
+router.post("/", ctrlWrapper(ctrl.addContact));
 
 router.post("/", validateBody(schemas.add), ctrlWrapper(ctrl.addContact));
 
