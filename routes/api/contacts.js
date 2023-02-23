8cb45dcd61141732f54de860b1f06dad3dc1a0cb
Igor Melnyk <igor.melnyk2011@gmail.com>
2023-02-23 21:45:28 +0200

    testing post method

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,23 +1,25 @@
 const express = require("express");
 
-const { ctrlWrapper } = require("../../helpers");
-
 const ctrl = require("../../controllers/contacts");
 
-// const {validateBody} = require("../../middlewares");
+const { ctrlWrapper } = require("../../helpers");
 
-// const schemas = require("../../schemas/contacts/");
+const {validationBody} = require("../../middlewares");
+
+const schemas = require("../../schemas/contacts");
+
+
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(ctrl.getAll));
+// router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+// router.get("/:contactId", ctrlWrapper(ctrl.getById));
 
-router.post("/", ctrlWrapper(ctrl.addContact));
+router.post("/", validationBody(schemas.add), ctrlWrapper(ctrl.addContact));
 
-router.delete("/:contactId", ctrlWrapper(ctrl.removeContactById));
+// router.delete("/:contactId", ctrlWrapper(ctrl.removeContactById));
 
-router.put("/:contactId", ctrlWrapper(ctrl.updateContactById));
+// router.put("/:contactId", ctrlWrapper(ctrl.updateContactById));
 
 module.exports = router;
