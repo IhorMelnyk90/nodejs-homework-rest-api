9a307c72e1b6f12cb075a7205b23d027806dada0
Igor Melnyk <igor.melnyk2011@gmail.com>
2023-03-11 20:06:52 +0200

    hw_v1

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const register = require("./registerUser");
 const login = require("./login");
 const current = require("./current");
 const logout = require("./logout");
+const updateAvatar = require("./updateAvatar");
 
 module.exports = {
   register,
   login,
   current,
   logout,
+  updateAvatar,
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,32 @@
 const express = require("express");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { userSchemas } = require("../../models");
 const { ctrlWrapper } = require("../../helpers");
 const ctrl = require("../../controllers/users");
 
 const router = express.Router();
 
+router.post(
+  "/register",
+  validateBody(userSchemas.registerSchema),
+  ctrlWrapper(ctrl.register)
+);
 
-router.post("/register", validateBody(userSchemas.registerSchema), ctrlWrapper(ctrl.register));
-
-router.post("/login", validateBody(userSchemas.loginSchema), ctrlWrapper(ctrl.login));
+router.post(
+  "/login",
+  validateBody(userSchemas.loginSchema),
+  ctrlWrapper(ctrl.login)
+);
 
 router.get("/current", authenticate, ctrlWrapper(ctrl.current));
 
 router.post("/logout", authenticate, ctrlWrapper(ctrl.logout));
 
-
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateAvatar)
+);
 
 module.exports = router;
